95ebfba9334361c16f7cd78347e32618a30dc40b
Joe King <j.king92@gmail.com>
2023-02-23 14:34:11 -0700

    feat: Add list model with relationships and relationship tests

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe List do
+  describe 'relationships' do
+    it { should belong_to :user }
+    it { should have_many :media_lists }
+    it { should have_many(:user_medias).through(:media_lists) }
+  end
+  
+end
