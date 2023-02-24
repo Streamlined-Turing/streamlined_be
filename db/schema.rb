6eb1d8d45bf08fb53405f996a3be378f5f45b8aa
Kelsie Porter <krporter22@gmail.com>
2023-02-23 18:45:37 -0700

    Update users table to include additional columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_23_232745) do
+ActiveRecord::Schema.define(version: 2023_02_24_014436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,10 +41,14 @@ ActiveRecord::Schema.define(version: 2023_02_23_232745) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "uid"
+    t.string "image"
   end
 
   add_foreign_key "lists", "users"
