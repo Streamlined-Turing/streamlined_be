8356d1c36d251b744ec98aac0b05ce96093e005f
Kelsie Porter <krporter22@gmail.com>
2023-02-23 18:53:02 -0700

    Set username default to ""

---
+++ db/migrate/20230224015005_change_default_value_for_user.rb
@@ -0,0 +1,5 @@
+class ChangeDefaultValueForUser < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default :users, :username, ""
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_24_014436) do
+ActiveRecord::Schema.define(version: 2023_02_24_015005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2023_02_24_014436) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "username"
+    t.string "username", default: ""
     t.string "uid"
     t.string "image"
   end
