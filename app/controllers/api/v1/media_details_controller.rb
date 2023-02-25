2797232e108c835748239ee6c982d9db1122f669
Joe King <j.king92@gmail.com>
2023-02-24 17:42:43 -0700

    remove comment

--- app/controllers/api/v1/media_details_controller.rb
+++ app/controllers/api/v1/media_details_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::MediaDetailsController < ApplicationController 
 
   def show 
-    # require 'pry'; binding.pry
     media = MediaFacade.details(params[:id])
     render json: MediaSerializer.new(media), status: :ok
   end
