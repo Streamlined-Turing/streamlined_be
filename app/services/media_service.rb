31d32fb07123c5572abcb2f85413fd4009a0f948
Joe King <j.king92@gmail.com>
2023-02-25 15:08:54 -0700

    feat: Add MediaService#search

--- app/services/media_service.rb
+++ app/services/media_service.rb
@@ -1,6 +1,15 @@
 class MediaService
   def self.details(id)
     response = conn.get("/v1/title/#{id}/details")
+    parse_json(response)
+  end
+
+  def self.search(query)
+    response = conn.get('/v1/search/', { search_field: 'name', search_value: query, types: 'tv,movie' })
+    parse_json(response)
+  end
+
+  def self.parse_json(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 
