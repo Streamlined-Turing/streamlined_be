06ad7fa8f2a26546dd19c4d94e1b2ba08ece6977
Joe King <j.king92@gmail.com>
2023-02-24 17:39:16 -0700

    feat: Add the media poros, serializer, service, and fixture file for mocking the reponse

---
+++ app/poros/media.rb
@@ -0,0 +1,32 @@
+class Media
+  attr_reader :id,
+              :title,
+              :audience_score,
+              :rating,
+              :type,
+              :description,
+              :genres,
+              :release_year,
+              :runtime,
+              :language,
+              :streaming_services,
+              :poster
+
+  def initialize(media_data)
+    @id                 = media_data[:id]
+    @title              = media_data[:title]
+    @audience_score     = media_data[:user_rating]
+    @rating             = media_data[:us_rating]
+    @type               = media_data[:type]
+    @description        = media_data[:plot_overview]
+    @genres             = media_data[:genre_names]
+    @release_year       = media_data[:year]
+    @runtime            = media_data[:runtime_minutes]
+    @language           = media_data[:original_language]
+    @streaming_services = media_data[:sources]
+    @poster             = media_data[:poster]
+  end
+
+  # make a method to find the streaming services wher type is 'sub' 
+  # or 'free' maybe? 
+end
