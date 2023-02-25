c7566e0dda5865976936166fb8f6474c1db776ce
Kelsie Porter <krporter22@gmail.com>
2023-02-25 10:40:43 -0700

    Add sad path test for incorrect media id

--- spec/requests/api/v1/media_details/media_details_request_spec.rb
+++ spec/requests/api/v1/media_details/media_details_request_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Watchmode API' do 
   describe 'media details request' do 
-    before(:each) do
-      stub_request(:get, "https://api.watchmode.com/v1/title/3173903/details?apiKey=#{ENV["watch_mode_api_key"]}")
-        .to_return(status: 200, body: File.read('spec/fixtures/breaking_bad_details_3173903.json'), headers: {})
-    end
-
     it 'receives media details' do 
+      stub_request(:get, "https://api.watchmode.com/v1/title/3173903/details?apiKey=#{ENV["watch_mode_api_key"]}")
+      .to_return(status: 200, body: File.read('spec/fixtures/breaking_bad_details_3173903.json'), headers: {})
+
       show_id = 3173903
       get "/api/v1/media_details/#{show_id}"
 
@@ -51,6 +49,24 @@ RSpec.describe 'Watchmode API' do
         expect(service).to be_a String
       end
       expect(media_data[:data][:attributes][:poster]).to be_a String
+    end
+
+    it 'returns an error message when an incorrect id is passed' do 
+      stub_request(:get, "https://api.watchmode.com/v1/title/show_id/details?apiKey=#{ENV["watch_mode_api_key"]}")
+      .to_return(status: 404, body: File.read('spec/fixtures/media_details_404.json'), headers: {})
+
+      show_id = 3173903
+      get "/api/v1/media_details/show_id"
+
+      media_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      expect(media_data).to eq({
+                                success: false,
+                                statusCode: 404,
+                                statusMessage: "The resource could not be found."
+                              })
     end
   end
 end
