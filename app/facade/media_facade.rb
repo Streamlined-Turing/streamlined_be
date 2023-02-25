32406ac9677a1d58b8b63c39874f902eb55264eb
Joe King <j.king92@gmail.com>
2023-02-25 15:08:19 -0700

    feat: Add facade method to create media poros from search

--- app/facade/media_facade.rb
+++ app/facade/media_facade.rb
@@ -3,4 +3,11 @@ class MediaFacade
     media_data = MediaService.details(id)
     Media.new(media_data) if media_data[:id]
   end
+
+  def self.search(query)
+    search_results = MediaService.search(query)
+    search_results[:title_results].map do |media_data|
+      Media.new(media_data)
+    end
+  end
 end
