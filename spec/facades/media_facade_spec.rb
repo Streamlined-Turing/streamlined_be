ede775a16abbe1799b50e8638c9766f1fa6f89e2
Joe King <j.king92@gmail.com>
2023-02-25 16:02:29 -0700

    test: Facade #search method

--- spec/facades/media_facade_spec.rb
+++ spec/facades/media_facade_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe MediaFacade do
     @breaking_bad_id = 3173903
     stub_request(:get, "https://api.watchmode.com/v1/title/3173903/details?apiKey=#{ENV['watch_mode_api_key']}&append_to_response=sources")
       .to_return(status: 200, body: File.read('spec/fixtures/breaking_bad_details_3173903.json'), headers: {})
+    
+    @query = 'everything'
+    stub_request(:get, "https://api.watchmode.com/v1/search/?search_field=name&search_value=#{@query}&types=tv,movie&apiKey=#{ENV['watch_mode_api_key']}")
+      .to_return(status: 200, body: File.read('spec/fixtures/media_search_everything.json'), headers: {})  
   end
 
   describe '.details' do
@@ -13,6 +17,16 @@ RSpec.describe MediaFacade do
 
       expect(media_details).to be_a Media
       expect(media_details.id).to eq(@breaking_bad_id)
+    end
+  end
+
+  describe '.search' do
+    it 'returns an array of media objects' do
+      search_results = MediaFacade.search(@query)
+
+      search_results.each do |result|
+        expect(result).to be_a Media
+      end
     end
   end
 end
