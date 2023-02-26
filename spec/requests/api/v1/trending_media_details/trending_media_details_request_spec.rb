4adadc7a492b2ac9f1ab9f45e8dcfcdbdfb30136
Kelsie Porter <krporter22@gmail.com>
2023-02-25 20:01:21 -0700

    Create test for trending media service

--- spec/requests/api/v1/trending_media_details/trending_media_details_request_spec.rb
+++ spec/requests/api/v1/trending_media_details/trending_media_details_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'MovieDB API' do
   describe 'trending media details request' do 
     it 'receives the days top 3 trending media details' do 
       stub_request(:get, "https://api.themoviedb.org/trending/all/day?api_key=#{ENV["moviedb_api_key"]}")
-      .to_return(status: 200, body: File.read('spec/fixtures/top_trending_media.json'), headers: {})
+        .to_return(status: 200, body: File.read('spec/fixtures/top_trending_media.json'), headers: {})
 
       get "/api/v1/trending_media"
 
