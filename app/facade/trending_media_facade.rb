50dbffb6803fbd6941537ca9f893e8cd9f631ea6
Kelsie Porter <krporter22@gmail.com>
2023-02-25 18:29:52 -0700

    Create trending media facade

---
+++ app/facade/trending_media_facade.rb
@@ -0,0 +1,12 @@
+class TrendingMediaFacade 
+
+  def self.details
+    media = TrendingMediaService.details 
+    media = media[:results].select do |media|
+      media[:media_type] == 'movie' || 'tv'
+    end
+    media[:results][0..2].map do |media|
+      TrendingMedia.new(media)
+    end
+  end
+end
