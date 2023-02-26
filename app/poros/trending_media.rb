0acb527f087ead637d8a1a573e3c4f6c473458d5
Kelsie Porter <krporter22@gmail.com>
2023-02-25 18:30:11 -0700

    Create trending media poro

---
+++ app/poros/trending_media.rb
@@ -0,0 +1,15 @@
+class TrendingMedia 
+  attr_reader :id,
+              :title,
+              :poster_path,
+              :media_type,
+              :vote_average
+
+  def initialize(media_params)
+    @id             = media_params[:id]
+    @title          = media_params[:title]
+    @poster_path    = media_params[:poster_path]
+    @media_type     = media_params[:media_type]
+    @vote_average   = media_params[:vote_average]
+  end
+end
