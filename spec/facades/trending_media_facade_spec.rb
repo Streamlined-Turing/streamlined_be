c2518747bf9f195cc8998e28993c24771d0c7059
Kelsie Porter <krporter22@gmail.com>
2023-02-25 19:58:50 -0700

    Test for trending media facade

---
+++ spec/facades/trending_media_facade_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe TrendingMediaFacade do 
+  before(:each) do 
+    stub_request(:get, "https://api.themoviedb.org/trending/all/day?api_key=#{ENV["moviedb_api_key"]}")
+      .to_return(status: 200, body: File.read('spec/fixtures/top_trending_media.json'), headers: {})
+  end
+
+  describe '.details' do 
+    it 'returns an array of 3 trending media objects' do 
+      media_array = TrendingMediaFacade.details
+
+      expect(media_array.count).to eq(3)
+      media_array.each do |media|
+        expect(media).to be_a TrendingMedia
+      end
+    end
+  end
+end
