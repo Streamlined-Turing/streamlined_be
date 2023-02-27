1bd8f698808c1bc717a20a254c3fceb523086d10
Joe King <j.king92@gmail.com>
2023-02-27 08:18:53 -0700

    test: media request response if given no value to query

--- spec/requests/api/v1/media/media_request_spec.rb
+++ spec/requests/api/v1/media/media_request_spec.rb
@@ -108,5 +108,20 @@ RSpec.describe 'Watchmode API' do
       expect(response).to be_successful
       expect(no_result).to eq({ :data => [] })
     end
+
+    it 'returns an error message if no query key words are passed' do
+      query = ''
+
+      get "/api/v1/media?q=#{query}"
+      missing_value_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(400)
+      expect(missing_value_response).to eq({
+                                            :success=>false, 
+                                            :statusCode=>400, 
+                                            :statusMessage=>"Must pass a value to query."
+                                          })
+    end
   end
 end
