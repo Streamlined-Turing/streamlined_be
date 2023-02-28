4caa7c2906560daf00bb9b33ec7acd67fd4572f1
Alex Pitzel <pitzelalex@gmail.com>
2023-02-28 12:59:33 -0500

    refactor: Update trending_media_facade_spec to use VCR

--- spec/facades/trending_media_facade_spec.rb
+++ spec/facades/trending_media_facade_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TrendingMediaFacade do
-  before(:each) do
-    stub_request(:get, "https://api.themoviedb.org/3/trending/all/day?api_key=#{ENV["moviedb_api_key"]}")
-      .to_return(status: 200, body: File.read('spec/fixtures/top_trending_media.json'), headers: {})
-  end
-
+RSpec.describe TrendingMediaFacade, :vcr do
   describe '.details' do
     it 'returns an array of 3 trending media objects' do
       media_array = TrendingMediaFacade.details
