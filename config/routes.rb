7f19d8955abd60c41742ba4952d1ab200fee52c9
Thomas Casady <tscasady@gmail.com>
2023-02-28 15:02:23 -0600

    Add route for user/media patch request.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, except: :index
-      resources :media, only: [:show, :index]
+      resources :users, except: :index do
+        resources :media, only: [:update], to: 'users/media#update'
+      end
+      resources :media, only: [:show, :index, :update]
       resources :trending_media, only: :index
     end
   end
