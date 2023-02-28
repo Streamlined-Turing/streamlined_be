8a55338d7605dea69bb5357457a9ff1b6b82b6df
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-27 23:52:40 -0600

    add sad path tests for showing a user list

--- spec/facades/media_facade_spec.rb
+++ spec/facades/media_facade_spec.rb
@@ -29,4 +29,32 @@ RSpec.describe MediaFacade do
       end
     end
   end
+
+  describe '.user_list_details' do
+    it 'returns an array of media objects on a particular users list' do
+      user = create(:user)
+      currently_watching_list = user.lists.find_by("name ILIKE ?", "currently watching")
+      breaking_bad = create(:user_media, media_id: 3173903)
+      MediaList.create(list: currently_watching_list, user_media: breaking_bad)
+
+
+      currently_watching_list = MediaFacade.user_list_details(user.id, "currently watching")
+
+      currently_watching_list.each do |media|
+        expect(media).to be_a(Media)
+      end
+    end
+
+    it 'returns an empty array if list has no media' do
+      user = create(:user)
+      currently_watching_list = MediaFacade.user_list_details(user.id, "currently watching")
+      expect(currently_watching_list).to be_empty
+    end
+
+    it 'returns an empty array if list name does not exist' do
+      user = create(:user)
+      not_a_list = MediaFacade.user_list_details(user.id, "not a list")
+      expect(not_a_list).to be_empty
+    end
+  end
 end
