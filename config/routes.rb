e97789bb50404c6f1d609800e1682e79397cbd95
Thomas Casady <tscasady@gmail.com>
2023-02-28 16:36:24 -0600

    Add route for post usermedia request.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, except: :index do
-        resources :media, only: [:update], to: 'users/media#update'
+        resources :media, only: [:create, :update], to: 'users/media#update'
         resources :lists, only: :index, controller: 'users/lists'
       end
       resources :media, only: [:show, :index]
