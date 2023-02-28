be1a4401ddca892fedcbb39f18a62067de25eea8
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-27 23:54:08 -0600

    add conditional exception raiser to user lists index

--- app/controllers/api/v1/users/lists_controller.rb
+++ app/controllers/api/v1/users/lists_controller.rb
@@ -1,2 +1,11 @@
+class Api::V1::Users::ListsController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    if user.has_list_name?(params[:list])
       list = MediaFacade.user_list_details(params[:user_id], params[:list])  
       render json: MediaSerializer.new(list), status: :ok
+    else
+      raise ActiveRecord::RecordNotFound.new "Couldn't find List with the name: '#{params[:list]}'"
+    end
+  end
+end
