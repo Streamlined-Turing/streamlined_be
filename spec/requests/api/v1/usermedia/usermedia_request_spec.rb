3216b2166cf92eadff4a6eef5d6d90e62b7b52ee
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-03-01 16:57:02 -0600

    add happy path spec for delete user media from list

--- spec/requests/api/v1/usermedia/usermedia_request_spec.rb
+++ spec/requests/api/v1/usermedia/usermedia_request_spec.rb
@@ -30,4 +30,23 @@ RSpec.describe 'Edit Media List API' do
       expect(user.user_medias.last.list.name).to eq "Want to Watch"
     end
   end
+  describe 'delete user_media/id' do
+    it 'deletes user_media for a user' do
+      user = create(:user) 
+      list_name = 'currently watching'
+      list = user.lists.find_by("name ILIKE ?", list_name)
+      user_media = create(:user_media, media_id: 3173903) 
+      media_list = MediaList.create(list: list, user_media: user_media)
+
+      expect(user.currently_watching).to eq([user_media])
+      expect(MediaList.find_by(list: list)).to eq(media_list)
+
+      delete "/api/v1/users/#{user.id}/media/#{user_media.media_id}"
+      
+      expect(response).to be_successful
+      expect(response.status).to eq 204
+      
+      expect(MediaList.find_by(list: list)).to be nil
+      expect(user.currently_watching).to eq([])
+    end
 end
