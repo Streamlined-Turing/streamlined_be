8323c5cdb55158ae46f97a0dc17059e4e067458c
Joe King <j.king92@gmail.com>
2023-05-08 15:56:55 -0700

    chore: Add route for sidekiq view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+  
   namespace :api do
     namespace :v1 do
       resources :users, except: :index do
