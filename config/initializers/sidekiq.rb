b018d101f6bc511ded47e561fd9518ed3968eb1d
Joe King <j.king92@gmail.com>
2023-05-17 09:32:04 -0700

    Update env variable name and add redis gem

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['redis_url'] }
+  config.redis = { url: ENV['REDIS_ENDPOINT_URI'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['redis_url'] }
+  config.redis = { url: ENV['REDIS_ENDPOINT_URI'] }
 end
