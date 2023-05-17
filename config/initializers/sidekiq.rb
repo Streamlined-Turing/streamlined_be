845bd1d3a391093479ca0adde086effe767be681
Joe King <j.king92@gmail.com>
2023-05-17 09:52:05 -0700

    Add redis initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_ENDPOINT_URI'] }
+  config.redis = {
+    url: ENV["REDIS_ENDPOINT_URI"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_ENDPOINT_URI'] }
+  config.redis = {
+    url: ENV["REDIS_ENDPOINT_URI"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
